e3ddb37a17996f5527666f6ac67209dad4170d32
Samuel Pérez <samuelperezsantiago12@gmail.com>
2025-03-24 17:28:40 +0100

    Fixed the Error messages

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: samperez <samperez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/17 11:19:09 by samperez          #+#    #+#             */
-/*   Updated: 2025/03/24 15:21:36 by samperez         ###   ########.fr       */
+/*   Updated: 2025/03/24 16:26:47 by samperez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,9 @@ int	main(int argc, char **argv)
 
 	map = ft_calloc(1, sizeof(t_game));
 	if (!map)
-		return (ft_printf("ERROR\nMalloc failed\n"));
+		return (ft_printf("Error\nMalloc failed\n"));
 	if (argc != 2)
-		return (ft_printf("ERROR\nIncorrect number of arguments\n"));
+		return (ft_printf("Error\nIncorrect number of arguments\n"));
 	fd = check_map(argv[1]);
 	read_map(map, fd);
 	free_all(map);
--- src/parsing.c
+++ src/parsing.c
@@ -6,7 +6,7 @@
 /*   By: samperez <samperez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/17 12:37:41 by samperez          #+#    #+#             */
-/*   Updated: 2025/03/24 15:19:56 by samperez         ###   ########.fr       */
+/*   Updated: 2025/03/24 16:27:08 by samperez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,13 +21,13 @@ int	check_map(const char *map_file)
 	if (len <= 4 || map_file[len - 1] != 'r' || map_file[len - 2] != 'e'
 		|| map_file[len - 3] != 'b' || map_file[len - 4] != '.')
 	{
-		ft_printf("ERROR\nWRONG MAP FORMAT\n");
+		ft_printf("Error\nWRONG MAP FORMAT\n");
 		exit(EXIT_FAILURE);
 	}
 	fd = open(map_file, O_RDONLY);
 	if (fd == -1)
 	{
-		ft_printf("ERROR\nCAN'T OPEN THE MAP");
+		ft_printf("Error\nCAN'T OPEN THE MAP");
 		exit(EXIT_FAILURE);
 	}
 	return (fd);
